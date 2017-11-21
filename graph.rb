7b49f2931ecdc414b48727da993daab6ab6c1fe8
zacgeis <zacgeis@gmail.com>
2017-11-20 21:25:38 -0600

    Add initial version of solver

--- graph.rb
+++ graph.rb
@@ -9,11 +9,11 @@ class Node
     @id = opts[:id] || "#{self.class.name}-#{SecureRandom.uuid[0..7]}"
   end
 
-  def resolve(context)
+  def resolve(*args)
     raise NotImplementedError
   end
 
-  def backward(context)
+  def backward(*args)
     raise NotImplementedError
   end
 end
@@ -26,12 +26,11 @@ class Multiply < Node
     in2.out_nodes << self
   end
 
-  def resolve(context)
-    product = @in_nodes[0].resolve(context) * @in_nodes[1].resolve(context)
-    context[@id] = product
+  def resolve(x, y)
+    return x * y
   end
 
-  def backward(context)
+  def backward
     raise NotImplementedError
   end
 end
@@ -44,12 +43,11 @@ class Add < Node
     in2.out_nodes << self
   end
 
-  def resolve(context)
-    sum = @in_nodes[0].resolve(context) + @in_nodes[1].resolve(context)
-    context[@id] = sum
+  def resolve(x, y)
+    return x + y
   end
 
-  def backward(context)
+  def backward
     raise NotImplementedError
   end
 end
@@ -59,20 +57,71 @@ class Value < Node
     super(opts)
   end
 
-  def resolve(context)
-    context[@id]
+  def resolve
+    raise NotImplementedError
   end
 
-  def backward(context)
+  def backward
     raise NotImplementedError
   end
 end
 
+class Solver
+  attr_accessor :sorted_nodes
+
+  def initialize(result_node, initial_context)
+    @result_node = result_node
+    @context = {}.merge(initial_context)
+    @sort_visited = {}
+    @sorted_nodes = []
+    sort_nodes(@result_node)
+  end
+
+  def sort_nodes(node)
+    if @sort_visited[node.id] == 1
+      raise "equation graph is not a dag"
+    end
+    if @sort_visited[node.id] == 2
+      return
+    end
+    @sort_visited[node.id] = 1
+    node.in_nodes.each do |child_node|
+      sort_nodes(child_node)
+    end
+    @sort_visited[node.id] = 2
+    @sorted_nodes.push(node)
+  end
+
+  def resolve
+    last_value = nil
+    @sorted_nodes.each do |node|
+      if node.is_a?(Value)
+        raise "'#{node.id}' value not found" if @context[node.id].nil?
+      else
+        inputs = node.in_nodes.map { |in_node| @context[in_node.id] }
+        result = node.resolve(*inputs)
+        @context[node.id] = result
+      end
+      last_value = @context[node.id]
+    end
+    last_value
+  end
+end
 
 x = Value.new(id: "x", static: true)
 m = Value.new(id: "m")
 b = Value.new(id: "b")
 graph = Add.new(Multiply.new(m, x), b)
+
+context = {
+  "x" => 2.0,
+  "m" => 1.0,
+  "b" => 1.0,
+}
+
+solver = Solver.new(graph, context)
+puts solver.sorted_nodes
+puts solver.resolve
 
 # TODO:
 # Keep all of the state in context and don't let it bleed to the actual network
@@ -85,12 +134,6 @@ graph = Add.new(Multiply.new(m, x), b)
 # raise exception if they can't be found in the context
 # Do we need to be careful about going over nodes twice
 
-context = {
-  "x" => 2.0,
-  "m" => 1.0,
-  "b" => 1.0,
-}
-
-r = graph.resolve(context)
-puts context
-puts r
+# r = graph.resolve(context)
+# puts context
+# puts r
